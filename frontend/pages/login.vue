3ba544dab0e3b9942d4126fc242127d1580434ad
JingKritsada <jing.kritsada@gmail.com>
2024-12-10 20:25:04 +0700

    understand code

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -7,14 +7,13 @@ useHead({
     title: 'Login',
 });
 
-// TODO: 1. variables
-// const normalVariable = 1;
-// const reactiveVariable = ref(1);
+const normalVariable = 1;
+const reactiveVariable = ref(1);
+// reactiveVariable ค่าจะเปลี่ยนแปลงได้ และจะทำให้ component ที่ใช้ reactiveVariable นั้น re-render ใหม่
 
-// TODO: 2. console
-// console.log('normalVariable', normalVariable);
-// console.log('reactiveVariable', reactiveVariable);
-// console.log('reactiveVariable.value', reactiveVariable.value);
+console.log('normalVariable', normalVariable);
+console.log('reactiveVariable', reactiveVariable);
+console.log('reactiveVariable.value', reactiveVariable.value);
 
 const onSubmit = async (values) => {
     console.log('values', values);
@@ -25,22 +24,20 @@ const onSubmit = async (values) => {
     <main class="m-auto w-[300px]">
         <LoginForm @submit="onSubmit" />
 
-        <!-- TODO: 3. Normal variable -->
-        <!-- <p class="mt-10">normal: {{ normalVariable }}</p>
+        <p class="mt-10">normal: {{ normalVariable }}</p>
         <BaseButton
             size="small"
             @click="normalVariable = normalVariable + 1"
         >
             normal variable
-        </BaseButton> -->
+        </BaseButton>
 
-        <!-- TODO: 4. Normal variable -->
-        <!-- <p class="mt-10">reactive: {{ reactiveVariable }}</p>
+        <p class="mt-10">reactive: {{ reactiveVariable }}</p>
         <BaseButton
             size="small"
             @click="reactiveVariable = reactiveVariable + 1"
         >
             reactive variable
-        </BaseButton> -->
+        </BaseButton>
     </main>
 </template>
