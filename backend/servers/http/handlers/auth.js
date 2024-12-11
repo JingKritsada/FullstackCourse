61195f0f61c4b18df9bc8681c4cf5ccdb294c33d
JingKritsada <jing.kritsada@gmail.com>
2024-12-11 22:02:46 +0700

    understand code

--- backend/servers/http/handlers/auth.js
+++ backend/servers/http/handlers/auth.js
@@ -2,18 +2,16 @@ import express from 'express';
 import asyncHandler from 'express-async-handler';
 
 export default ({ }, config) => {
-    // TODO: 19. router of auth
     function router() {
         const router = express.Router();
 
-        // TODO: 19.5 async handler
+        // asyncHandler คือ middleware ที่จะทำการจัดการ error ให้เรา
         router.post('/login', asyncHandler(login));
         router.post('/register', asyncHandler(register));
 
         return router;
     }
 
-    // TODO: 20. Login handler
     async function login(req, res) {
         res.status(200).json({
             token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlBpY2hldCBJdG5nYW0iLCJpYXQiOjE1MTYyMzkwMjJ9.mVMnU3cgNFO27o_i1xxiYptVpa3ruv1H_mKJD3qzfj0',
@@ -21,7 +19,6 @@ export default ({ }, config) => {
         });
     }
 
-    // TODO: 21. Register handler
     async function register(req, res) {
         res.status(200).json({
             status: true,
